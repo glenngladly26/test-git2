c20ce7d649fbbe46c0777732b576b4b477e236f0
glenngladly26 <glenngladlygessong@gmail.com>
2024-09-16 17:01:12 +0800

    m

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D player;
+    public float jumpForce = 5f;
+    public float moveSpeed = 40f;
+    public float direction;
+    public float jumpCooldown = 100f; //Adjust jump Cooldown
+    private bool canJump = true;
+    private bool moveLeft, moveRight;
+    public bool crouch;
+
+    public Animator animator;
+
+    //inputSystem script
+    PlayerControls controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        controls.Enable();
+
+        controls.Player.Move.performed += info =>
+        {
+            direction = info.ReadValue<float>();
+        };
+    }
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        player.velocity = new Vector2(direction * moveSpeed * Time.deltaTime, player.velocity.y);
+
+        if (moveLeft)
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+
+        if (moveRight)
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+
+        if (Mathf.Abs(player.velocity.y) < 0.1f)
+        {
+            canJump = true;
+        }
+    }
+
+    public void JumpInput()
+    {
+        if (canJump)
+        {
+            player.velocity = new Vector2(player.velocity.x, jumpForce);
+            StartCoroutine(JumpCooldown());
+        }
+
+    }
+
+    IEnumerator JumpCooldown()
+    {
+        canJump = false;
+        yield return new WaitForSeconds(jumpCooldown);
+        canJump = true;
+    }
+
+
+    public void CrouchInputButtonDown()
+    {
+        crouch = true;
+    }
+
+    public void CrouchInputButtonUp()
+    {
+        crouch = false;
+    }
+
+    public void LeftInputButtonDown()
+    {
+        //kiri
+        moveLeft = true;
+        direction = -1;
+    }
+
+    public void LeftInputButtonUp()
+    {
+        moveLeft = false;
+    }
+
+    public void RightInputButtonDown()
+    {
+        //kanan
+        moveRight = true;
+        direction = 1;
+    }
+
+    public void RightInputButtonUp()
+    {
+        moveRight = false;
+
+    }
+}
